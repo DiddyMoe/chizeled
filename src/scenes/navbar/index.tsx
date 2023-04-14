0985210b697d8225afd14ad2c3bb14e40c26a9bb
Mohammad Saad <msaad10792@gmail.com>
2023-04-13 23:29:46 -0500

    Added clickable and responsive navbar functionality

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
-type Props = {}
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-const index = (props: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
   return (
-    <div>index</div>
-  )
-}
+    <nav>
+      <div
+        className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* Left side of Navbar */}
+            <img alt="logo" src={Logo} />
 
-export default index
+            {/* Right side of Navbar */}
+            <div className={`${flexBetween} w-full`}>
+              {/* Inner Left side of Navbar with the links */}
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link 
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                  />
+              </div>
+              {/* Inner Right side of Navbar with the Sign in / Join links */}
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
