f8e891c50bb1c02582e577d75631f1f42c1f2a6b
Mohammad Saad <msaad10792@gmail.com>
2023-04-14 19:24:18 -0500

    Added more navbar reactivity using MediaQuery

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px");
   return (
     <nav>
       <div
@@ -21,6 +25,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <img alt="logo" src={Logo} />
 
             {/* Right side of Navbar */}
+            {isAboveMediumScreens ? (
             <div className={`${flexBetween} w-full`}>
               {/* Inner Left side of Navbar with the links */}
               <div className={`${flexBetween} gap-8 text-sm`}>
@@ -50,9 +55,15 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 <p>Sign In</p>
                 <button>Become a Member</button>
               </div>
-
             </div>
-
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
 
